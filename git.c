fa7fd341dc4b3f88ba8b4db58d540cffeb2b0447
spenserdaniel <spenserbhavan@gmail.com>
2020-02-25 16:15:30 +0530

    File Added

---
+++ git.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+float radius,area;
+float p=3.14;
+printf("radius=");
+scanf("%f",&radius);
+area=p*radius*radius;
+printf("area of the circle=%f\n",area);
+return 0;
+} 
+

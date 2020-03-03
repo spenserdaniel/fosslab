e3b3bdc92bd5325c4ba976c6f8133544917bc733
spenserdaniel <spenserbhavan@gmail.com>
2020-03-03 15:15:35 +0530

    done

---
+++ fosslab/sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,s;
+printf("enter the first no\n");
+scanf("%d",&a);
+printf("Enter the second no\n");
+scanf("%d",&b);
+s=a+b;
+printf("sum of the nos=%d\n",s);
+return 0;
+}
